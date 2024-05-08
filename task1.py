ca0d22996efb59d8badb681442b79997f731774f
Nandup9753 <126007350+Nandup9753@users.noreply.github.com>
2024-05-08 23:05:51 +0530

    intial

--- task1.py
+++ task1.py
@@ -1,21 +1,8 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Define the range of x values
-x = np.linspace(-5, 5, 100)
-
-# Define the equation
-y = x**2 + 2*x + 1
-
-# Create the plot
-plt.plot(x, y, color='red')
-plt.title('Parabola')
-plt.xlabel('X')
-plt.ylabel('Y')
-
-# Set the axis limits
-plt.xlim(-5, 5)
-plt.ylim(-5, 10)
-
-# Show the plot
-plt.show()
+def half_pyramid(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print("* ", end="")
+        print("\r")
+ 
+n = 5
+half_pyramid(n)
